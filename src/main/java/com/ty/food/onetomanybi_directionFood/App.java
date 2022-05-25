345cd8a7939ee8471925370de1d603d2e0781275
venki121998 <vkvenkatesh1998@gmail.com>
2022-05-25 15:32:55 -0700

    onetomantbif

---
+++ src/main/java/com/ty/food/onetomanybi_directionFood/App.java
@@ -0,0 +1,13 @@
+package com.ty.food.onetomanybi_directionFood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
